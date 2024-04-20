fb944c05ed2aeeb6445fc75fb472c1f1f28907d6
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2024-04-20 18:45:08 +0300

    another build test

--- app/src/test/java/com/kiryuha21/jobsearchplatformclient/ExampleUnitTest.kt
+++ app/src/test/java/com/kiryuha21/jobsearchplatformclient/ExampleUnitTest.kt
@@ -1,7 +1,5 @@
 package com.kiryuha21.jobsearchplatformclient
 
-import android.net.http.HttpException
-import android.util.Log
 import com.kiryuha21.jobsearchplatformclient.data.domain.UserRole
 import com.kiryuha21.jobsearchplatformclient.data.remote.AuthToken
 import com.kiryuha21.jobsearchplatformclient.data.remote.RetrofitObject
@@ -26,35 +24,31 @@ class ExampleUnitTest {
 
     @Test
     fun createGetDeleteUser() = runTest {
-        try {
-            val userRetrofit = RetrofitObject.retrofit.create(UserAPI::class.java)
-            val user = UserDTO.SignUpUserDTO(
-                email = "test@gmail.com",
-                username = "my_example_login",
-                password = "secure_one",
-                role = UserRole.Worker
-            )
-
-            val newUser = userRetrofit.createNewUser(user)
-            val createdUser = userRetrofit.getUserByUsername("my_example_login")
-            // assertEquals(newUser, createdUser)
-
-            AuthToken.createToken("my_example_login", "secure_one")
-            // assertNotNull(AuthToken.getToken())
-
-            userRetrofit.deleteUserByUsername("my_example_login", "Bearer ${AuthToken.getToken()}")
-            userRetrofit.getUserByUsername("my_example_login")
-//            assertThrows(Exception::class.java) {
-//                runTest {
-//                    userRetrofit.getUserByUsername("my_example_login")
-//                }
-//            }
-        } catch (e: HttpException) {
-            println("HERE: reason is ${e.message}")
-            e.message?.let { Log.d("tag1", it) }
+        val userRetrofit = try {
+            RetrofitObject.retrofit.create(UserAPI::class.java)
         } catch (e: Exception) {
-            println("HERE: reason is ${e.message}")
+            println(e.message)
+            throw Exception(e.message)
         }
+        val user = UserDTO.SignUpUserDTO(
+            email = "test@gmail.com",
+            username = "my_example_login",
+            password = "secure_one",
+            role = UserRole.Worker
+        )
 
+        val newUser = userRetrofit.createNewUser(user)
+        val createdUser = userRetrofit.getUserByUsername("my_example_login")
+        assertEquals(newUser, createdUser)
+
+        AuthToken.createToken("my_example_login", "secure_one")
+        assertNotNull(AuthToken.getToken())
+
+        userRetrofit.deleteUserByUsername("my_example_login", "Bearer ${AuthToken.getToken()}")
+        assertThrows(Exception::class.java) {
+            runTest {
+                userRetrofit.getUserByUsername("my_example_login")
+            }
+        }
     }
 }
