0916f2b3a6a51d42040d984c008db6d2ace6b2a6
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2024-04-27 22:51:16 +0300

    returned tests in workflow hoping everything is gonna be okay

--- app/src/test/java/com/kiryuha21/jobsearchplatformclient/ExampleUnitTest.kt
+++ app/src/test/java/com/kiryuha21/jobsearchplatformclient/ExampleUnitTest.kt
@@ -2,9 +2,8 @@ package com.kiryuha21.jobsearchplatformclient
 
 import com.kiryuha21.jobsearchplatformclient.data.domain.UserRole
 import com.kiryuha21.jobsearchplatformclient.data.remote.AuthToken
-import com.kiryuha21.jobsearchplatformclient.data.remote.RetrofitObject
-import com.kiryuha21.jobsearchplatformclient.data.remote.api.UserAPI
 import com.kiryuha21.jobsearchplatformclient.data.remote.dto.UserDTO
+import com.kiryuha21.jobsearchplatformclient.data.remote.RetrofitObject.userRetrofit
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
@@ -24,7 +23,6 @@ class ExampleUnitTest {
 
     @Test
     fun createGetDeleteUser() = runTest {
-        val userRetrofit = RetrofitObject.retrofit.create(UserAPI::class.java)
         val user = UserDTO.SignUpUserDTO(
             email = "test@gmail.com",
             username = "my_example_login",
@@ -33,16 +31,16 @@ class ExampleUnitTest {
         )
 
         val newUser = userRetrofit.createNewUser(user)
-        val createdUser = userRetrofit.getUserByUsername("my_example_login")
+        val createdUser = userRetrofit.getUserByUsername(user.username)
         assertEquals(newUser, createdUser)
 
-        AuthToken.createToken("my_example_login", "secure_one")
+        AuthToken.createToken(user.username, user.password)
         assertNotNull(AuthToken.getToken())
 
-        userRetrofit.deleteUserByUsername("my_example_login", "Bearer ${AuthToken.getToken()}")
+        userRetrofit.deleteUserByUsername(user.username, "Bearer ${AuthToken.getToken()}")
         assertThrows(Exception::class.java) {
             runTest {
-                userRetrofit.getUserByUsername("my_example_login")
+                userRetrofit.getUserByUsername(user.username)
             }
         }
     }
