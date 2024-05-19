083213622f235d36dd068338fc0523986a373f15
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2024-05-19 15:49:45 +0300

    removed redundant action from action file

--- app/src/main/java/com/kiryuha21/jobsearchplatformclient/ui/navigation/NavigationDrawerItems.kt
+++ app/src/main/java/com/kiryuha21/jobsearchplatformclient/ui/navigation/NavigationDrawerItems.kt
@@ -8,7 +8,7 @@ import androidx.compose.material.icons.filled.Settings
 import com.kiryuha21.jobsearchplatformclient.data.domain.NavigationDrawerItem
 
 val navigationDrawerItems = listOf(
-    NavigationDrawerItem(Icons.Default.Home, "Вакансии", NavigationGraph.MainApp.HOME_SCREEN),
+    NavigationDrawerItem(Icons.Default.Home, "Главная", NavigationGraph.MainApp.HOME_SCREEN),
     NavigationDrawerItem(Icons.Default.AccountCircle, "Профиль", NavigationGraph.MainApp.PROFILE),
     NavigationDrawerItem(Icons.Default.ChatBubble, "Предложения", NavigationGraph.MainApp.JOB_APPLICATIONS),
     NavigationDrawerItem(Icons.Default.Settings, "Настройки", NavigationGraph.MainApp.SETTINGS)
